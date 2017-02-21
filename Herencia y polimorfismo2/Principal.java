86dc38a0f8f09dd1e9a453a6439de9ff444051fa
Jonathanbaro <jonathan.baro21@hotmail.com>
2017-02-21 12:01:49 -0600

    Polimorfismo
    Se agrego un arreglo figuras y con un ciclo manda llamar los metodos
    necesarios

--- Herencia y polimorfismo2/Principal.java
+++ Herencia y polimorfismo2/Principal.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Principal here.
  * 
@@ -9,14 +9,28 @@ public class Principal
 {
     public static void main(String [] args)
     {
+
         Triangulo t1 = new Triangulo(3,4);
-        t1.calculaArea();
-        System.out.println("El area del triangulo es: " + t1.accedeArea());
+        //t1.calculaArea();
+        //System.out.println("El area del triangulo es: " + t1.accedeArea());
         Cuadrado c1 = new Cuadrado(4);
-        c1.calculaArea();
-        System.out.println("El area del cuadrado es: " + c1.accedeArea());
+        //c1.calculaArea();
+        //System.out.println("El area del cuadrado es: " + c1.accedeArea());
         Circulo ci1 = new Circulo(9);
-        ci1.calculaArea();
-        System.out.println("El area del circulo es: " + ci1.accedeArea());
+        //ci1.calculaArea();
+        //System.out.println("El area del circulo es: " + ci1.accedeArea());
+        Figura [] figuras = new Figura[3];
+        figuras[0] = t1;
+        figuras[1] = c1;
+        figuras[2] = ci1;
+        for(int i = 0; i<3; i++)
+        {
+           figuras[i].calculaArea();
+        }
+        for(int i = 0; i<3; i++)
+        {
+            System.out.println("El area de la figura " + i + " es " + figuras[i].accedeArea());
+        }
+        
     }
 }
