ba875e45deb545ad1431901b47f4e02bf2b2d8b7
Jonathanbaro <jonathan.baro21@hotmail.com>
2017-03-01 11:59:28 -0600

    Intento 2 de agregar figuras
    aun no hay resultados

--- Herencia y polimorfismo2/Lienzo.java
+++ Herencia y polimorfismo2/Lienzo.java
@@ -1,6 +1,7 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 /**
  * Write a description of class Lienzo here.
@@ -13,14 +14,15 @@ public class Lienzo extends JPanel
     private ArrayList<Figura> figuras;
     public Lienzo()
     {
-        
+        ArrayList<Figura> figuras = new ArrayList<Figura>();
     }
     @Override
     public void paintComponent(Graphics g)
     {
-        Graphics2D g2 = (Graphics2D)g;
-        g2.drawString("Hola MUNDO ", 100, 200);
-        g2.drawRect(20, 40, 40, 80);
+        for(Figura f : figuras)
+        {
+            f.dibuja(g);
+        }
     }
     
 }
