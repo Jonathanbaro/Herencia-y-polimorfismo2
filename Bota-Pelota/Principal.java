3146191a26d59d5d66498c0ea78c4e5ece24ae93
Jonathanbaro <jonathan.baro21@hotmail.com>
2017-03-09 12:03:09 -0600

    Pelota
    Hacer que se mueva una pelota

---
+++ Bota-Pelota/Principal.java
@@ -0,0 +1,11 @@
+
+
+public class Principal
+{
+    public static void main(String [] args)
+    {
+        Ventana windows = new Ventana(800, 600);
+        windows.setTitle("Bota Pelota");
+        windows.setVisible(true);
+    }
+}
